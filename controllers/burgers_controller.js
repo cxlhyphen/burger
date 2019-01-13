5f52ffa784a2c82e608e1e68dde25ee1ddb81b8f
cindy <42073246+cxlhyphen@users.noreply.github.com>
2019-01-13 05:08:45 -0500

    added favicon fx

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,11 @@ const express = require("express");
 const router = express.Router();
 const burger = require("../models/burger.js");
 
+//favicon :(
+router.get("/favicon.ico", function(req, res) {
+  res.status(204);
+});
+
 //Base route
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
