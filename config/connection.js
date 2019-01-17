7bab880e4b6f3d1061fede742d54da755c618180
cindy <42073246+cxlhyphen@users.noreply.github.com>
2019-01-16 21:50:25 -0500

    fixing heroku issue

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 //Stores connection to mySQL database
 //Dependencies
 const mysql = require("mysql");
-//require("dotenv").config();
+require("dotenv").config();
 
 //create connection to db
-const connection;
+const connection = mysql.createConnection({
+  port: 8889,
+  host: "localhost",
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "burgers_db"
+});
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    port: 8889,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-  });
-}
+  connection = connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+
+// else {
+//   connection = mysql.createConnection({
+//     port: 8889,
+//     host: "localhost",
+//     user: "root",
+//     password: process.env.PASSWORD,
+//     database: "burgers_db"
+//   });
+// }
 
 connection.connect(function(err) {
   if (err) throw err;
